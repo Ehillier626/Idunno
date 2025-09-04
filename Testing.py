02efa901d39a5876fe6738d0e494895911919e0b
Erc Hillier <ehillier@academic.rrc.ca>
2025-09-04 08:54:24 -0500

    Added Addition function

--- Testing.py
+++ Testing.py
@@ -1,1 +1,13 @@
-print('hello world')
+import streamlit as st
+import pandas as pd
+import numpy as np
+from Function import Addition
+st.title("Select a date")
+
+a = st.slider("select Number", 1, 100, key="a")
+
+b = st.slider("select Number", 1, 100, key="b")
+
+Addition(a, b)
+
+st.write("The addition is", Addition(a, b))
